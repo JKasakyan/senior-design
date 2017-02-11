568105d4e6c46b7776e1df61e5e05ee4c07902d5
JKasakyan <jameskasakyan94@gmail.com>
2017-02-11 13:20:32 -0500

    Template for script that crawls through jsons and processes each tweet

---
+++ src/process_tweets.py
@@ -0,0 +1,29 @@
+"""
+process_tweets.py
+
+Template for processing tweets
+"""
+
+
+import glob
+import argparse
+
+from json_io import list_to_json, list_from_json
+
+
+if __name__ == "__main__":
+    # Setup CLA parser
+    parser = argparse.ArgumentParser(description='Process tweets')
+    parser.add_argument('path', help='path to directory containing tweet jsons. Needs trailing "/"')
+    # parse CLAs
+    args = parser.parse_args()
+    JSON_DIR = args.path
+
+    # Populate list with paths to jsons
+    json_paths_lst = glob.glob(JSON_DIR + "*.json")
+
+    # Process tweets
+    # processed_tweets = [ process(tweet) for json_path in json_paths_lst for tweet in list_from_json(json_path)]
+
+    # Save processed tweets to json
+    # list_to_json(processed_tweets, path/to/save/tweets.json)
