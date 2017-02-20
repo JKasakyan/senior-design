a1eb8a6de762138236a0113fb58076f3fab02cba
JKasakyan <jameskasakyan94@gmail.com>
2017-02-20 16:39:18 -0500

    use json_io function, minor doctstring fix

--- src/squash_tweets.py
+++ src/squash_tweets.py
@@ -1,17 +1,16 @@
 """
 squash_tweets.py
 
-USAGE:
-$ python squash_tweets.py [-h] [--parse] path
+USAGE: squash_tweets.py [-h] [--delete] path
 
-Squash multiple jsons into one.
+Squash multiple jsons into one
 
 positional arguments:
   path        path to directory containing tweet jsons. Needs trailing "/"
 
 optional arguments:
   -h, --help  show this help message and exit
-  --parse     use if jsons to squash need to be parsed first
+  --delete    delete old JSONs after squashing
 """
 
 import sys
@@ -19,9 +18,7 @@ import os
 import glob
 import argparse
 
-from datetime import datetime
-
-from json_io import list_to_json, list_from_json
+from json_io import list_to_json, list_from_json, merge_json_filenames
 
 
 if __name__ == "__main__":
@@ -35,17 +32,13 @@ if __name__ == "__main__":
         raise Exception("{} is not a valid path.".format(args.path))
 
     # Populate list with paths to jsons
-    json_paths_lst = glob.glob(args.path + "*.json")
+    json_paths_lst = glob.glob(args.path + "*-*-*_*-*-*.json")
 
     # create one list of all merged tweets
     merged_lst = [ tweet for json_path in json_paths_lst for tweet in list_from_json(json_path)]
 
     # Get earliest and latest date of jsons squashed for naming purposes of merged file.
-    parse_date_from_filename = lambda fn: fn.split('/')[-1].split('.')[0].split('_')
-    sorted_dates = sorted([datetime.strptime(date, "%Y-%m-%d") for fn in json_paths_lst for date in parse_date_from_filename(fn)])
-    from_date = datetime.strftime(sorted_dates[0], "%Y-%m-%d")
-    to_date = datetime.strftime(sorted_dates[-1], "%Y-%m-%d")
-    filename = "{}_{}.json".format(from_date, to_date)
+    filename = merge_json_filenames(json_paths_lst)
 
     # Save merged list to json
     list_to_json(merged_lst, args.path + filename)
